bd4160e48c526d9817e093b2ae4c6a497da454a0
adamknapp124 <aknapp124@gmail.com>
2023-10-28 13:06:57 -0700

    added conditional for side scroll on stories component

--- app/components/Stories.js
+++ app/components/Stories.js
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 import rightArrow from '../../public/images/icons/story-card-icons/right-arrow.svg';
@@ -7,18 +9,21 @@ import leftArrow from '../../public/images/icons/story-card-icons/left-arrow.svg
 import StoryCard from './StoryCard';
 
 const Stories = () => {
+	const [showButton, setShowButton] = useState(false);
 	return (
 		<div className="flex gap-3 w-[600px] relative">
 			<div className="absolute inset-0 flex items-center z-10">
-				<div className="bg-white absolute hidden left-3 h-[48px] w-[48px] rounded-full flex justify-center">
-					<Image
-						src={leftArrow}
-						alt="left arrow"
-						height={12}
-						width={12}
-						className="my-auto"
-					/>
-				</div>
+				{showButton ? (
+					<div className="bg-white absolute left-3 h-[48px] w-[48px] rounded-full flex justify-center">
+						<Image
+							src={leftArrow}
+							alt="left arrow"
+							height={12}
+							width={12}
+							className="my-auto"
+						/>
+					</div>
+				) : null}
 				<div className="bg-white absolute right-3 h-[48px] w-[48px] rounded-full items-center flex justify-center">
 					<Image
 						src={rightArrow}
