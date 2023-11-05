ee6f6223104ea763a298ed23278edbcaa6094e80
adamknapp124 <aknapp124@gmail.com>
2023-11-04 23:02:08 -0700

    updated schema for posts

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@
 
 import { useSession } from 'next-auth/react';
 import { useState, useEffect } from 'react';
+import { redirect } from 'next/navigation';
 
 import SideNav from '/app/components/SideNav';
 import Feed from '/app/components/Feed';
 import ContactPanel from '/app/components/ContactPanel';
 import Navbar from '/app/components/Navbar';
+// import { redirect } from 'next/dist/server/api-utils';
 
 export default function Home() {
 	const [twoColumn, setTwoColumn] = useState(false);
@@ -28,12 +30,15 @@ export default function Home() {
 	useEffect(() => {
 		handleResize();
 		window.addEventListener('resize', handleResize);
+		if (!session) {
+			redirect('/login');
+		}
 
 		// Clean up the event listener when the component unmounts
 		return () => {
 			window.removeEventListener('resize', handleResize);
 		};
-	}, [twoColumn]);
+	});
 
 	const { data: session, status } = useSession();
 	console.log(session);
